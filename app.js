28e4475267d8372a1cb08bbe4f781ddc269f7dd3
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-18 20:52:12 -0300

    API Led on/off

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+var express = require('express');
+var app = express();
 var five = require('johnny-five');
 var board = new five.Board({port: "COM3"});
 
 board.on("ready", function(){
-    var led = new five.Led(9);
-    led.blink(500);
+    led = new five.Led(9);
 })
+
+app.get('/on', function(req, res){
+    led.on();
+    res.send({'on':true});
+});
+
+app.get('/off', function(req, res){
+    led.off();
+    res.send({'on':false});
+});
+
+    app.listen(3000, function(){
+        console.log('Escutando na porta 3000');
+});
